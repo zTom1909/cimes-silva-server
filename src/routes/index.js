6c0d8961a5c5999336176c9a852e25b44c68832b
zTom1909 <topolococo2@gmail.com>
2023-09-27 19:14:30 -0300

    feat(*): Added routes management

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const userRouter = require("./user");
+
+const router = Router();
+
+router.use("/user", userRouter);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const router = require("./routes");
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(cors());
 server.use(express.json());
+
+server.use("/api", router);
 
 module.exports = server;
