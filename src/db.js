983901a61890db86205ddbcbfe2b1893e4a273d0
zTom1909 <topolococo2@gmail.com>
2023-12-17 00:30:00 -0300

    feat(db): Added connections to the new models

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 const config = require("./utils/config");
 const UserModel = require("./models/User");
+const AdminModel = require("./models/Admin")
+const ProductModel = require("./models/Product")
 
 const { database } = config;
 const { host, name, password, user } = database;
@@ -8,6 +10,8 @@ const URL = `postgres://${user}:${password}@${host}/${name}`;
 
 const sequelize = new Sequelize(URL, { logging: false, native: false });
 UserModel(sequelize);
+AdminModel(sequelize)
+ProductModel(sequelize)
 
 module.exports = {
   sequelize,
