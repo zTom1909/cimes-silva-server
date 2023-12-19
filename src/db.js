96a49e437cdb22024f95eab579e772545b5c1898
zTom1909 <topolococo2@gmail.com>
2023-12-19 17:58:50 -0300

    fix(db): Fixed not supporting ssl postgre dbs

--- src/db.js
+++ src/db.js
@@ -1,17 +1,31 @@
 const { Sequelize } = require("sequelize");
 const config = require("./utils/config");
 const UserModel = require("./models/User");
-const AdminModel = require("./models/Admin")
-const ProductModel = require("./models/Product")
+const AdminModel = require("./models/Admin");
+const ProductModel = require("./models/Product");
 
-const { database } = config;
+const { database, type } = config;
 const { host, name, password, user, url } = database;
 const URL = url || `postgres://${user}:${password}@${host}/${name}`;
+const sqlConfig =
+  type === "production"
+    ? {
+        logging: false,
+        native: false,
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : { logging: false, native: false };
 
-const sequelize = new Sequelize(URL, { logging: false, native: false });
+const sequelize = new Sequelize(URL, sqlConfig);
 UserModel(sequelize);
-AdminModel(sequelize)
-ProductModel(sequelize)
+AdminModel(sequelize);
+ProductModel(sequelize);
 
 module.exports = {
   sequelize,
