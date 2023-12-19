3d9293ea697c7808f26c7a8138d0c0b0b12d340f
zTom1909 <topolococo2@gmail.com>
2023-12-19 18:05:35 -0300

    fix(index): Fixed using the same variable name for the server file and server object from the config file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { sequelize } = require("./src/db");
-const server = require("./src/server");
+const app = require("./src/server");
 
 const { type, server } = require("./src/utils/config");
 const isProduction = type === "production";
@@ -9,6 +9,6 @@ sequelize
   .sync({ force: !isProduction })
   .then(() => {
     console.log("Database synced correctly");
-    server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
   })
   .catch((error) => console.error(error));
