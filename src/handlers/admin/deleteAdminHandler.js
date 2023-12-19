50c22e02994f13668078ee85dd5c0dbcf2bf05ad
zTom1909 <topolococo2@gmail.com>
2023-12-19 17:16:56 -0300

    fix(admin endpoints): Fixed 2 endpoints showing the password in the returned object

--- src/handlers/admin/deleteAdminHandler.js
+++ src/handlers/admin/deleteAdminHandler.js
@@ -1,7 +1,10 @@
 const { Admin } = require("../../db");
 const deleteAdminHandler = async (id) => {
   await Admin.update({ isDisabled: true }, { where: { id } });
-  return await Admin.findByPk(id);
+  return await Admin.findOne({
+    where: { id },
+    attributes: { exclude: ["password"] },
+  });
 };
 
 module.exports = deleteAdminHandler;
--- src/handlers/admin/editAdminHandler.js
+++ src/handlers/admin/editAdminHandler.js
@@ -3,7 +3,10 @@ const editAdminHandler = async (id, properties) => {
   const validValues = Object.values(properties).filter((value) => value !== undefined);
   if (!validValues.length) throw new Error("Properties to edit are empty.");
   await Admin.update(properties, { where: { id } })
-  return await Admin.findByPk(id);
+  return await Admin.findOne({
+    where: { id },
+    attributes: { exclude: ["password"] },
+  });
 };
 
 module.exports = editAdminHandler;
