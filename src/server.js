2fd0901a8aa1077911b007a15f7a59111ee40cc8
zTom1909 <topolococo2@gmail.com>
2024-01-30 00:45:50 -0300

    feat(*): Added rate limit to all requests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const cors = require("cors");
 const authMiddleware = require("./utils/authMiddleware");
@@ -22,10 +23,30 @@ const corsOptions = {
   exposedHeaders: "X-Total-Count",
 };
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  message: "You've sent too many requests, please try again later.",
+});
+
+const checkWhitelist = (req) => {
+  const clientIp =
+    req.ip ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+  return process.env.ADMIN_ORIGIN === clientIp;
+};
+
 server.use(cors(corsOptions));
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(authMiddleware);
+server.use(limiter);
+server.use((req, res, next) => {
+  if (checkWhitelist(req)) return next();
+  return limiter(req, res, next);
+});
 
 server.use("/api", router);
 
